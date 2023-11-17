d6f55f4322ce3982419bdca6b0f453886e3aecd3
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-11-16 19:48:09 -0800

    early return invert condition

--- tabs-are-windows.js
+++ tabs-are-windows.js
@@ -2,14 +2,16 @@
     browser.tabs.onCreated.addListener(async function (tab) {
 	// If we let the first tab of any window trigger this, it
 	// would keep triggering itself infinitely.
-	if (tab.index > 0) {
-	    const newW = await browser.windows.create();
-
-	    // Move the tab to the last position in the new window
-	    await browser.tabs.move([tab.id], {windowId: newW.id, index: -1});
-
-	    // Close the first tab
-	    await browser.tabs.remove(newW.tabs[0].id);
+	if (tab.index == 0) {
+	    return;
 	}
+
+	const newW = await browser.windows.create();
+
+	// Move the tab to the last position in the new window
+	await browser.tabs.move([tab.id], {windowId: newW.id, index: -1});
+
+	// Close the first tab
+	await browser.tabs.remove(newW.tabs[0].id);
     });
 })();
